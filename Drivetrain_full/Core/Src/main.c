ac82edd77107f480a6acffb536b86497cbfc2d47
Bix <erikssonbix@gmail.com>
2021-11-16 14:53:00 +0100

    added thread for thrust controll

--- Drivetrain_full/Core/Src/main.c
+++ Drivetrain_full/Core/Src/main.c
@@ -46,6 +46,7 @@
 SPI_HandleTypeDef hspi1;
 
 TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim3;
 
 /* Definitions for StartEncoder */
 osThreadId_t StartEncoderHandle;
@@ -61,6 +62,13 @@ const osThreadAttr_t StartStepper_attributes = {
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityNormal,
 };
+/* Definitions for StartThrust */
+osThreadId_t StartThrustHandle;
+const osThreadAttr_t StartThrust_attributes = {
+  .name = "StartThrust",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityBelowNormal,
+};
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -70,8 +78,10 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_TIM1_Init(void);
+static void MX_TIM3_Init(void);
 void ReadEncoder(void *argument);
 void ControlStepper(void *argument);
+void ControlThrust(void *argument);
 
 /* USER CODE BEGIN PFP */
 
@@ -86,8 +96,13 @@ uint8_t SPI_rx[2];
 
 uint16_t INITIAL_ANGLE = 0;
 static uint16_t ENCODER_ANGLE = 0;
-static int16_t ANGLE_REF = 0;
+static int16_t ANGLE_REF = 30;
 static int16_t ANGLE_ERROR;
+
+static float MAX_THRUST = 50.0;
+static uint16_t THRUST_DUTY_CYCLE;
+static int16_t THRUST_REF = 0;
+
 
 const float ConvertToDegree = 360.0/16383;
 float ANGLE_DEGREE;
@@ -102,6 +117,7 @@ uint16_t PWM_DUTY_CYCLE;
 
 int thread1 = 0;
 int thread2 = 0;
+int thread3 = 0;
 int status = 0;
 
 /* USER CODE END 0 */
@@ -136,8 +152,11 @@ int main(void)
   MX_GPIO_Init();
   MX_SPI1_Init();
   MX_TIM1_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
+
   HAL_Delay(10);
   //INITIALIZE PIN STATE AS HIGH
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
@@ -178,6 +197,9 @@ int main(void)
 
   /* creation of StartStepper */
   StartStepperHandle = osThreadNew(ControlStepper, NULL, &StartStepper_attributes);
+
+  /* creation of StartThrust */
+  StartThrustHandle = osThreadNew(ControlThrust, NULL, &StartThrust_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -360,6 +382,65 @@ static void MX_TIM1_Init(void)
 }
 
 /**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 0;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 65535;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+  HAL_TIM_MspPostInit(&htim3);
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -369,16 +450,24 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOE_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOF, GPIO_PIN_4|GPIO_PIN_14, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOF, GPIO_PIN_14, GPIO_PIN_RESET);
+  /*Configure GPIO pins : PF4 PF14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PA4 */
   GPIO_InitStruct.Pin = GPIO_PIN_4;
@@ -386,13 +475,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PF14 */
-  GPIO_InitStruct.Pin = GPIO_PIN_14;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
 
 }
 
@@ -431,7 +513,7 @@ void ReadEncoder(void *argument)
 	}
 	ANGLE_ERROR = ANGLE_REF - ANGLE_DEGREE; //CALCULATE ANGLE ERROR
 	thread1++;
-    osThreadFlagsWait(1U,osFlagsWaitAny, osWaitForever); // START ControlStepper thread
+    osThreadFlagsWait(0x01,osFlagsWaitAny, osWaitForever); // START ControlStepper thread
 
   }
   /* USER CODE END 5 */
@@ -450,7 +532,7 @@ void ControlStepper(void *argument)
   /* Infinite loop */
   for(;;)
   {
-	//TODO: Change direction depending on Error sign
+	//Change direction depending on Error sign
 	if (ANGLE_ERROR < 0){
 		HAL_GPIO_WritePin(GPIOF, GPIO_PIN_14, GPIO_PIN_RESET);
 		status = 100;
@@ -466,6 +548,8 @@ void ControlStepper(void *argument)
 	if(ANGLE_ERROR < 2){
 		PWM_DUTY_CYCLE = 0;
 		//TODO: CALL THRUST THREAD
+		osThreadFlagsSet(StartThrustHandle, 0x03);
+		osThreadFlagsWait(0x02,osFlagsWaitAny, osWaitForever);
 	}
 	TIM1->ARR  = PWM_PERIOD;
 	TIM1->CCR3 = PWM_DUTY_CYCLE;
@@ -479,9 +563,34 @@ void ControlStepper(void *argument)
 
 	osDelay(500U);
 	thread2++;
-	osThreadFlagsSet(StartEncoderHandle, 1U);
+	osThreadFlagsSet(StartEncoderHandle, 0x01);
   }
   /* USER CODE END ControlStepper */
+}
+
+/* USER CODE BEGIN Header_ControlThrust */
+/**
+* @brief Function implementing the StartThrust thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_ControlThrust */
+void ControlThrust(void *argument)
+{
+  /* USER CODE BEGIN ControlThrust */
+  /* Infinite loop */
+  for(;;)
+  {
+	//TIM3 ARR = 65535
+	osThreadFlagsWait(0x03,osFlagsWaitAny, osWaitForever);
+	THRUST_DUTY_CYCLE = (THRUST_REF / MAX_THRUST)*65535;
+	TIM3->CCR4 = THRUST_DUTY_CYCLE;
+	thread3++;
+    osDelay(1);
+    osThreadFlagsSet(StartStepperHandle, 0x02);
+
+  }
+  /* USER CODE END ControlThrust */
 }
 
 /**
